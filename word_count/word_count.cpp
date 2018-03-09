0adb5cd5edbfb21030fdc1226eff336c9d9db452
Break-Neck <breakneck11@gmail.com>
2018-03-09 13:24:08 +0000

    add cmd options

--- word_count/word_count.cpp
+++ word_count/word_count.cpp
@@ -3,10 +3,9 @@
 #include <unordered_map>
 #include <vector>
 
-constexpr int kWordsStartPos = 2;
-constexpr int kMinemalWordCount = 1000;
+#include "args/args.hxx"
 
-struct FastStringHasher {
+struct FasterStringHasher {
  public:
   constexpr static size_t kMultiplier = 17;
 
@@ -21,26 +20,29 @@ struct FastStringHasher {
 
 class Counter {
  public:
+  Counter(int minimal_frequency) : MinimalFrequncy_(minimal_frequency) {}
+
   void Up(const std::string& str) {
-    const auto it = count_map.find(str);
-    if (it == count_map.end()) {
-      count_map.insert({str, 1});
+    const auto it = CountMap_.find(str);
+    if (it == CountMap_.end()) {
+      CountMap_.insert({str, 1});
     } else {
-      if (++(it->second) == kMinemalWordCount) {
-        frequent_words.push_back(str);
+      if (++(it->second) == MinimalFrequncy_) {
+        FrequentWords_.push_back(str);
       }
     }
   }
 
-  const auto& GetFrequentWords() const noexcept { return frequent_words; }
+  const auto& GetFrequentWords() const noexcept { return FrequentWords_; }
 
   int GetCount(const std::string& str) const {
-    return count_map.find(str)->second;
+    return CountMap_.find(str)->second;
   }
 
  private:
-  std::unordered_map<std::string, int, FastStringHasher> count_map;
-  std::vector<std::string> frequent_words;
+  std::unordered_map<std::string, int, FasterStringHasher> CountMap_;
+  std::vector<std::string> FrequentWords_;
+  const int MinimalFrequncy_;
 };
 
 std::vector<std::string> Split(const std::string& str) {
@@ -58,22 +60,75 @@ std::vector<std::string> Split(const std::string& str) {
   return result;
 }
 
-int main() {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(nullptr);
+template <typename TCallback>
+void GetFrequentWordsFromStream(std::istream& input, int frequent_barrier,
+                                int skip_first_in_line,
+                                TCallback frequent_words_callback) {
   std::string line;
-  Counter word_counter;
-  while (std::getline(std::cin, line)) {
+  std::string tmp_buffer;
+  Counter word_counter(frequent_barrier);
+  while (std::getline(input, line)) {
     const auto splitted = Split(line);
-    for (int i = kWordsStartPos; i < splitted.size(); ++i) {
+    for (int i = skip_first_in_line; i < splitted.size(); ++i) {
       word_counter.Up(splitted[i]);
-      if (i > kWordsStartPos) {
-        word_counter.Up(splitted[i - 1] + '|' + splitted[i]);
+      if (i > skip_first_in_line) {
+        tmp_buffer = "";
+        tmp_buffer += splitted[i - 1];
+        tmp_buffer += '|';
+        tmp_buffer += splitted[i];
+        word_counter.Up(std::move(tmp_buffer));
       }
     }
   }
   for (const auto& it : word_counter.GetFrequentWords()) {
-    std::cout << it << " " << word_counter.GetCount(it) << "\n";
+    frequent_words_callback(it, word_counter.GetCount(it));
   }
+}
+
+bool TryFillParams(int argc, const char** argv, int* out_frequency,
+                   int* out_skip_first) {
+  constexpr int kDefaultMinimalWordCount = 1000;
+
+  args::ArgumentParser parser("Count words and bigrams and save counts");
+  args::HelpFlag help(parser, "help", "Display this help", {'h', "help"});
+  args::ValueFlag<int> skip(parser, "skip",
+                            "How many words skip in the beggining of the line",
+                            {'s', "skip"}, args::Options::Required);
+  args::ValueFlag<int> frequency(
+      parser, "frequency",
+      "Minimal frequency for a word or bigram to be printed", {'f', "freq"},
+      1000);
+
+  try {
+    parser.ParseCLI(argc, argv);
+  } catch (args::Help&) {
+    std::cout << parser;
+    return false;
+  } catch (const args::ParseError& e) {
+    std::cerr << e.what() << "\n";
+    std::cerr << parser;
+    return false;
+  } catch (const args::ValidationError& e) {
+    std::cerr << e.what() << "\n";
+    std::cerr << parser;
+    return false;
+  }
+
+  *out_frequency = args::get(frequency);
+  *out_skip_first = args::get(skip);
+  return true;
+}
+
+int main(int argc, const char** argv) {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  int minimal_frequency, skip_fisrt;
+  if (!TryFillParams(argc, argv, &minimal_frequency, &skip_fisrt)) {
+    return 1;
+  }
+  GetFrequentWordsFromStream(std::cin, minimal_frequency, skip_fisrt,
+                             [](const std::string& word, int count) {
+                               std::cout << word << " " << count << "\n";
+                             });
   return 0;
 }
