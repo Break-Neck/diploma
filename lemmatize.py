587f7fc7a36cf098b93715918aa5578c1030ec13
Break-Neck <breakneck11@gmail.com>
2018-03-14 19:48:16 +0000

    new lemmatizing

--- lemmatize.py
+++ lemmatize.py
@@ -3,38 +3,43 @@
 import sys
 import json
 
-from nltk.tokenize import casual_tokenize
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag
 from nltk.stem import WordNetLemmatizer
+
+__tag_convertion_map = {
+    'N' : 'n',
+    'V' : 'v',
+    'J' : 'a',
+    'S' : 's',
+    'R' : 'r'
+}
+__wnl = WordNetLemmatizer()
 
 
 def apply_not(lemmas, apply_length=2):
-    new_lemmas = []
     has_not_flag = 0
     for lemma in lemmas:
         if lemma == 'not':
             has_not_flag = apply_length
         elif has_not_flag > 0:
-            new_lemmas.append('not_' + lemma)
+            yield 'not_' + lemma
             has_not_flag -= 1
         else:
-            new_lemmas.append(lemma)
-    return new_lemmas
+            yield lemma
+
+
+def convert_tags(old_tag):
+    return __tag_convertion_map.get(old_tag[0], 'n')
 
 
 def get_lems(text):
-    tokens = casual_tokenize(text, preserve_case=False)
+    tokens = word_tokenize(text)
     for i, tok in enumerate(tokens):
         if tok.endswith("n't"):
             tokens[i] = 'not'
-    wnl = WordNetLemmatizer()
-
-    def dummy_lemmatize(token):
-        n_lemma = wnl.lemmatize(token, 'n')
-        v_lemma = wnl.lemmatize(token, 'v')
-        return n_lemma if len(n_lemma) <= len(v_lemma) else v_lemma
-
-    lemmas = [dummy_lemmatize(tok) for tok in tokens if tok.isalnum()]
-    return apply_not(lemmas)
+    tagged = pos_tag([tok.lower() for tok in tokens if tok.isalnum()])
+    return apply_not(__wnl.lemmatize(tok, convert_tags(tag)) for tok, tag in tagged)
 
 
 if __name__ == '__main__':
@@ -43,3 +48,4 @@ if __name__ == '__main__':
         date_str = json_data['date']
         text = json_data['text']
         print('{} {}'.format(date_str, ' '.join(get_lems(text))))
+
