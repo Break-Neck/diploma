8157eff66710480691a76303ef13749ab5f5b4b4
Break-Neck <breakneck11@gmail.com>
2018-03-18 20:17:15 +0000

    add td idf for count2matrix

--- count2matrix.py
+++ count2matrix.py
@@ -1,28 +1,49 @@
 #!/usr/bin/env python3
 
 import sys
+import argparse
 import numpy as np
 
 
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description='Generate and save count matrix')
+    parser.add_argument('words_path', help='Path to file with good words. ')
+    parser.add_argument(
+        'counts_path', help='Path to file with counted wobject.')
+    parser.add_argument(
+        'output_path', help='Path to file to save matrix into. ')
+    parser.add_argument('--tf-idf', help='Calculate tf-idf matrix (word file need to contain document frequencies in 3rd column)',
+                        action='store_true', dest='tf_idf')
+    return parser
+
+
 def main():
-    if len(sys.argv) != 4:
-        print('Usage: ./count2matrix.py good_words_file day_count_file output_file')
-        sys.exit(1)
-    words_file, input_file, output_file = tuple(sys.argv[1:])
-    with open(input_file) as fl:
+    parser = get_parser()
+    args = parser.parse_args()
+    with open(args.counts_path) as fl:
         total_lines = sum(1 for _ in fl)
     print('Lines:', total_lines)
-    with open(words_file) as fl:
+    with open(args.words_path) as fl:
         words_indexes = {line.split()[0]: i for i, line in enumerate(fl)}
+        if args.tf_idf:
+            fl.seek(0)
+            document_frequencies = np.array(
+                [int(line.strip().split()[2]) for line in fl])
     print('Word-like objects: ', len(words_indexes))
+    print('Shape: ', total_lines * len(words_indexes))
     result_array = np.zeros((total_lines, len(words_indexes)))
-    with open(input_file) as fl:
+    with open(args.counts_path) as fl:
         for i, line in enumerate(fl):
             words_counts = line.split()[1:]
             for word, count_str in (tuple(pair.split(':')) for pair in words_counts):
                 result_array[i][words_indexes[word]] = int(count_str)
+    if args.tf_idf:
+        df_mul = np.log(total_lines / document_frequencies)
+        for i in range(result_array.shape[0]):
+            result_array[i] = np.multiply(result_array[i], df_mul)
     print('Nonzero: ', np.count_nonzero(result_array) / result_array.size)
-    np.save(output_file, result_array)
+    np.save(args.output_path, result_array)
 
 
 if __name__ == '__main__':
